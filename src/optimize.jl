e5be055102cfab8aa7a7a38479fb0fc51012c2cb
finalbossqc <mastercoder7200@gmail.com>
2025-05-22 21:03:29 -0400

    Updated optimize.jl

--- src/optimize.jl
+++ src/optimize.jl
@@ -3,8 +3,8 @@ using ADTypes
 
 export optimize
 
-function optimize(X₀::AbstractVector; include_imr=true)
-    f = OptimizationFunction(IMR_obj, grad=G_IMR_obj!, hess=H_IMR_obj!)
+function optimize(X₀::AbstractVector; include_imr=1)
+    f = OptimizationFunction((X,p) -> IMR_obj(X,p)*include_imr, grad=G_IMR_obj!, hess=H_IMR_obj!)
     prob = OptimizationProblem(f, X₀, 0.0)
     return solve(prob, LBFGS())
 end
